4bddc2119e6e91214ce7fd034117a68b0e82ef6f
Barnes, Dan <dbarnes1@paychex.com>
2021-02-26 13:00:55 -0500

    chore: use external/peer dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -38,6 +38,7 @@ module.exports = [
     {
         context: 'globalThis',
         input: 'index.mjs',
+        external: ['lodash-es', '@paychex/core'],
         plugins: [
             nodeResolve({
                 preferBuiltins: true,
